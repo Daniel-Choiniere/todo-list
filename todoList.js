b42de2bbe04a796c710405abc6c495a2cb99979d
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2018-11-10 12:14:30 +0000

    added toggle all functionality

--- todoList.js
+++ todoList.js
@@ -1,35 +1,80 @@
 var todoList = {
   todos: [],
   displayTodos: function() {
-    console.log('My Todos:');
-    for (var i = 0; i , this.todos.length; i++) {
-        console.log(this.todos[i].todoText);
+    //  if user tries to submit an empty Todo submisson
+    if (this.todos.length === 0) {
+      console.log('Your todo list is empty!');
+    }
+    else {
+      console.log('My Todos: ')
+      for (var i = 0; i < this.todos.length; i++) {
+        // if valid Todo added, make the property completed true, display x
+        if (this.todos[i].completed === true) {
+          console.log('(x)', this.todos[i].todoText);
+        // else if item not completed, no x is displayed
+        } else {
+          console.log('()', this.todos[i].todoText);
+        }
+      }
     }
   },
+  
   addTodo: function(todoText) {
     this.todos.push({
-        todoText: todoText,
-        completed: false
+      todoText: todoText,
+      completed: false
     });
     this.displayTodos();
   },
+  
   changeTodo: function(position, todoText) {
     this.todos[position].todoText = todoText;
     this.displayTodos();
   },
+  
   deleteTodo: function(position) {
     this.todos.splice(position, 1);
     this.displayTodos();
   },
+  
   toggleCompleted: function(position) {
-      var todo = this.todos[position];
-      todo.completed = !todo.completed;
-      this.displayTodos();
+    var todo = this.todos[position];
+    todo.completed = !todo.completed;
+    this.displayTodos();
+  },
+  
+  toggleAll: function() {
+    var totalTodos = this.todos.length;
+    var completedTodos = 0;
+    // Get number of completed (checked) Todos
+    for (var i = 0; i < totalTodos; i++) {
+      if (this.todos[i].completed === true) {
+        completedTodos++;
+      }
+    }
+    // If all Todos are completed, make everything false (not checked)
+    if (completedTodos === totalTodos) {
+      for (var i = 0; i < totalTodos; i++) {
+        this.todos[i].completed = false;
+      } 
+    //  If only some Todos are completed, make the rest true (checked)
+    } else {
+      for (var i = 0; i < totalTodos; i++) {
+        this.todos[i].completed = true;
+      }
+    }
+    this.displayTodos();
   }
 };
 
 
 console.log(todoList.addTodo('first'));
 console.log(todoList.addTodo('second'));
+console.log(todoList.addTodo('third'));
 
-// console.log(todoList.displayTodos());
+console.log(todoList.toggleCompleted(1));
+console.log(todoList.toggleCompleted(0));
+console.log(todoList.toggleCompleted(2));
+
+console.log(todoList.toggleAll())
+
